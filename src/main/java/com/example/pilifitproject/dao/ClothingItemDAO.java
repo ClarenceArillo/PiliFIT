7e7947d2f31fe2e6bdd4f6eb59708d4992b85509
CLARENCE <clarence.ar9@gmail.com>
2025-05-26 22:30:41 +0800

    Unchecked ClothingItemDAO & FitDAO

--- src/main/java/com/example/pilifitproject/dao/ClothingItemDAO.java
+++ src/main/java/com/example/pilifitproject/dao/ClothingItemDAO.java
@@ -7,15 +7,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClothingItemDAO {
-    public List<ClothingItem> getAllClothingItem() throws SQLException{
+    public List<ClothingItem> getAllClothingItem() throws SQLException {
         List<ClothingItem> items = new ArrayList<>();
         String sql = "SELECT * FROM clothing_item";
 
-        try(Connection conn = DBConnection.getConnection();
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql)){
+        try (Connection conn = DBConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
 
-            while(rs.next()){
+            while (rs.next()) {
                 ClothingItem item = new ClothingItem(
                         rs.getInt("id"),
                         rs.getString("name"),
@@ -32,11 +32,11 @@ public class ClothingItemDAO {
         return items;
     }
 
-    public void addClothingItem(ClothingItem item) throws SQLException{
+    public void addClothingItem(ClothingItem item) throws SQLException {
         String sql = "INSERT INTO clothing_item (name, image_path, category_id, color, style, size, is_favorite)" + "VALUES (?, ?, ?, ?, ?, ?, ?)";
 
-        try(Connection conn = DBConnection.getConnection();
-           PreparedStatement pstmt = conn.prepareStatement(sql)){
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
             pstmt.setString(1, item.getName());
             pstmt.setString(2, item.getImagePath());
@@ -55,37 +55,60 @@ public class ClothingItemDAO {
                 "style = ?, size = ?, is_favorite = ? WHERE id = ?";
 
         try (Connection conn = DBConnection.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)){
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-             pstmt.setString(1, item.getName());
-             pstmt.setInt(2, item.getCategoryId());
-             pstmt.setString(3, item.getColor());
-             pstmt.setString(4, item.getStyle());
-             pstmt.setString(5, item.getSize());
-             pstmt.setInt(6, item.getIsFavorite());
-             pstmt.setInt(7, item.getId());
-             pstmt.executeUpdate();
+            pstmt.setString(1, item.getName());
+            pstmt.setInt(2, item.getCategoryId());
+            pstmt.setString(3, item.getColor());
+            pstmt.setString(4, item.getStyle());
+            pstmt.setString(5, item.getSize());
+            pstmt.setInt(6, item.getIsFavorite());
+            pstmt.setInt(7, item.getId());
+            pstmt.executeUpdate();
 
         }
 
     }
 
-    public void deleteClothingItem(int id) throws SQLException{
+    public void deleteClothingItem(int id) throws SQLException {
         String sql = "DELETE FROM clothing_item WHERE id = ?";
 
-        try(Connection conn = DBConnection.getConnection();
-            PreparedStatement pstmt = conn.prepareStatement(sql))  {
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
             pstmt.setInt(1, id);
             pstmt.executeUpdate();
         }
     }
 
-//    public List<ClothingItem> getAllClothingItemsByCategory(int categoryId)throws SQLException{
-//        List<ClothingItem> items = new ArrayList<>();
-//
-//
-//    }
+    public List<ClothingItem> getAllClothingItemsByCategory(int categoryId) throws SQLException {
+        List<ClothingItem> items = new ArrayList<>();
+        String sql = "SELECT * FROM clothing_item WHERE category_id = ?";
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, categoryId);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+
+                    ClothingItem item = new ClothingItem(
+                            rs.getInt("id"),
+                            rs.getString("name"),
+                            rs.getString("image_path"),
+                            rs.getInt("category_id"),
+                            rs.getString("color"),
+                            rs.getString("style"),
+                            rs.getString("size"),
+                            rs.getInt("is_Favorite")
+                    );
+                    items.add(item);
+                }
+            }
+
+            return items;
+        }
 
 
+    }
 }
