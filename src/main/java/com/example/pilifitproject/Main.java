19bdfcf88c0469885ca52c20492d7ee862e74d7b
CLARENCE <clarence.ar9@gmail.com>
2025-06-12 15:05:41 +0800

    FitDialogController added

--- src/main/java/com/example/pilifitproject/Main.java
+++ src/main/java/com/example/pilifitproject/Main.java
@@ -23,7 +23,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException, SQLException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com/example/pilifitproject/view/Home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 768);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
---
+++ src/main/java/com/example/pilifitproject/controller/FitDialogController.java
@@ -0,0 +1,6 @@
+package com.example.pilifitproject.controller;
+
+public class FitDialogController {
+
+
+}
